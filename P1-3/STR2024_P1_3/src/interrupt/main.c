614c206581b2426350c9707839db790629de1a2a
naibu3 <pablipoyo@gmail.com>
2025-03-18 17:49:58 +0100

    aparcao

--- P1-3/STR2024_P1_3/src/interrupt/main.c
+++ P1-3/STR2024_P1_3/src/interrupt/main.c
@@ -8,18 +8,15 @@
 const char* TAG="P1_3";
 #define LED_GPIO GPIO_NUM_2
 
-static volatile bool btn_pressed = false;  // Bandera para evitar operaciones en ISR
-
 // Manejador de interrupción (ISR)
 static void IRAM_ATTR gpio_isr_handler(void* arg) {
-    ESP_DRAM_LOGI(TAG, "Botón pulsado!");
-    btn_pressed = !btn_pressed;  // Alternar estado del botón
-    gpio_set_level(LED_GPIO, btn_pressed ? 0 : 1);  // Invertir LED
+    ESP_DRAM_LOGI(TAG, "Botón pulsado! Ejecutando interrupcion");
+    gpio_set_level(LED_GPIO, !gpio_get_level(CONFIG_GPIO_PIN));  // Invertir LED
 }
 void app_main(void) {
     gpio_config_t io_conf = \
     {
-        .intr_type = GPIO_INTR_NEGEDGE,
+        .intr_type = GPIO_INTR_ANYEDGE,
         .mode = GPIO_MODE_INPUT,
         .pin_bit_mask = (1ULL<<CONFIG_GPIO_PIN),
         .pull_down_en = GPIO_PULLDOWN_DISABLE,
@@ -41,6 +38,7 @@ void app_main(void) {
     ESP_ERROR_CHECK(gpio_isr_handler_add(CONFIG_GPIO_PIN, gpio_isr_handler, NULL));
 
     while (1) {
+        //gpio_set_level(LED_GPIO, 1);  // Invertir LED
         vTaskDelay(10 / portTICK_PERIOD_MS);
     }
 }
