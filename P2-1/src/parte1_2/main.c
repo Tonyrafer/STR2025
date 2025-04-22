aaf798e91e9d07a6cd1bcacdc75e7cf0722fcf06
Tonyrafer <antonioraso99@gmail.com>
2025-04-22 16:04:23 +0200

    Actualización del main

--- P2-1/src/parte1_2/main.c
+++ P2-1/src/parte1_2/main.c
@@ -9,30 +9,36 @@ static const char *TAG = "P2_1_2";
 // Callback function for the timer that occurs every second
 void timer_callback_1(void *arg)
 {
-    uint64_t tiempo1 =  esp_timer_get_time();
+    int* timer1_ticks = (int*)arg;
+    (*timer1_ticks)++;
     char _str[50];
-    sprintf(_str, "Timer 1 Tick %.2f us. \n" ,(double)tiempo1);
+    sprintf(_str, "Timer 1 Tick nº %i. \n" ,*timer1_ticks);
     ESP_DRAM_LOGI(TAG,"%s", _str); // ESP_DRAM_LOGI(TAG,"%s", str);
 }
 // Callback function for the timer that occurs every 5 seconds
 void timer_callback_2(void *arg)
 {
-    uint64_t tiempo2 =  esp_timer_get_time();
+
+    int* timer2_ticks = (int*)arg;
+    (*timer2_ticks)++;
     char _str[50];
-    sprintf(_str, "Timer 2 Tick %.2f us. \n" ,(double)tiempo2);
+    sprintf(_str, "Timer 2 Tick nº %i . \n" ,*timer2_ticks);
     ESP_DRAM_LOGI(TAG,"%s", _str); // ESP_DRAM_LOGI(TAG,"%s", str);
 }
 
 void app_main(void)
 {
-    
+    int timer1_ticks = 0;
+    int timer2_ticks = 0;
+
     // Timer 1: estructuras manejador y configurador
     esp_timer_handle_t timer1;
     esp_timer_create_args_t timer_args1 =
         {
             .callback = &timer_callback_1, // callback de interrupción
-            .name = "timer_1"}
-            ;
+            .name = "timer_1",
+            .arg = &timer1_ticks
+        };
     // creación del timer 1, se pasa manejador y configurador. Al terminar este método, el manejador ya no es NULL
     esp_timer_create(&timer_args1, &timer1);
     // Uso del timer 1: interrupción periódica cada segundo (los timers están implementados en microsegundos)
@@ -43,13 +49,15 @@ void app_main(void)
     esp_timer_create_args_t timer_args2 =
         {
             .callback = &timer_callback_2,
-            .name = "timer_2"};
+            .name = "timer_2",
+            .arg = &timer2_ticks
+        };
     esp_timer_create(&timer_args2, &timer2);
     esp_timer_start_periodic(timer2, 5000000); // 5 segundos
     // Un bucle, para que la ejecución no se detenga
     while (1)
     {
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
+        while (1);
     }
     // Lo que habría que hacer al finalizar con los timers de forma controlada.
 
